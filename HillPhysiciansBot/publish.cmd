948cefb8b7d329319b3c2cdae61500cceb89729e
michacg <michacg2@uci.edu>
2019-09-09 13:53:56 -0700

    Prompt bot trial

--- HillPhysiciansBot/publish.cmd
+++
@@ -1,3 +0,0 @@
-nuget restore
-msbuild QnABot.sln -p:DeployOnBuild=true -p:PublishProfile=hillphysiciansqnaservice-bot-Web-Deploy.pubxml -p:Password=yGQTCB5RmM95NkfhEi3ib6KCFxX4jcGN28lmNT54bF9Wnxgw9ZvFh1FAu5qj
-
